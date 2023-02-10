e6107155b24cb766f9f22df9eae97625f542bfbe
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-02-10 16:24:30 +0900

    add：ViewModel追加

--- app/src/main/java/com/nokopi/marketregistersystem/MainActivity.kt
+++ app/src/main/java/com/nokopi/marketregistersystem/MainActivity.kt
@@ -2,10 +2,18 @@ package com.nokopi.marketregistersystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nokopi.marketregistersystem.databinding.ActivityMainBinding
+import com.nokopi.marketregistersystem.ui.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
     }
 }
---
+++ app/src/main/java/com/nokopi/marketregistersystem/ui/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.nokopi.marketregistersystem.ui
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+}
