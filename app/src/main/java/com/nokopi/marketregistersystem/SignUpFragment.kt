68acf0b23d9c8eff66a31ac8e256308d030b1b11
hiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-04-12 20:07:52 +0900

    add：新規登録画面とユーザー画面追加

---
+++ app/src/main/java/com/nokopi/marketregistersystem/SignUpFragment.kt
@@ -0,0 +1,24 @@
+package com.nokopi.marketregistersystem
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.nokopi.marketregistersystem.databinding.FragmentSignupBinding
+
+class SignUpFragment: Fragment() {
+
+    private lateinit var binding: FragmentSignupBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+}
