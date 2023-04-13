632b47f6031d39fed074143a73d702e51a99a860
hiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-04-13 18:03:55 +0900

    update：SignUpように名前を入力する欄を追加

--- app/src/main/java/com/nokopi/marketregistersystem/SignUpFragment.kt
+++ app/src/main/java/com/nokopi/marketregistersystem/SignUpFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.nokopi.marketregistersystem.data.UserDatabase
 import com.nokopi.marketregistersystem.databinding.FragmentSignupBinding
 
 class SignUpFragment: Fragment() {
 
     private lateinit var binding: FragmentSignupBinding
+    private lateinit var viewModel: SignUpViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +22,17 @@ class SignUpFragment: Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false)
         binding.lifecycleOwner = this
+        val application = requireNotNull(this.activity).application
+        val dataSource = UserDatabase.getInstance(application).userDatabaseDao
+        val viewModelFactory = SignUpViewModelFactory(dataSource)
+        viewModel = ViewModelProvider(this, viewModelFactory)[SignUpViewModel::class.java]
+        binding.signUpViewModel = viewModel
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
 }
