b8aad1ae800bd46fffb6bc6d6d439a826a0b913a
hiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-04-17 17:59:42 +0900

    feat：新規にユーザー情報をRoomに登録する機能を追加

--- app/src/main/java/com/nokopi/marketregistersystem/SignUpViewModel.kt
+++ app/src/main/java/com/nokopi/marketregistersystem/SignUpViewModel.kt
@@ -4,10 +4,12 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.nokopi.marketregistersystem.data.User
 import com.nokopi.marketregistersystem.data.UserDatabaseDao
+import kotlinx.coroutines.launch
 
-class SignUpViewModel(private val database: UserDatabaseDao): ViewModel() {
+class SignUpViewModel(private val database: UserDatabaseDao, private val inputId: String): ViewModel() {
 
     val userNameText: MutableLiveData<String> = MutableLiveData<String>("")
     private val userName: LiveData<String>
@@ -18,20 +20,24 @@ class SignUpViewModel(private val database: UserDatabaseDao): ViewModel() {
         get() = _isEnabled
 
 
-    fun signOnUser() {
+    fun signUpUser() {
+        val inputUserName = userNameText.value.toString()
+        viewModelScope.launch {
+            val user = User(
+                userName = inputUserName,
+                userId = inputId,
+                userBalance = 0
+            )
+            database.insert(user)
+        }
         Log.i("userName", userName.value.toString())
+        Log.i("userId", inputId)
     }
 
     fun updateButton() {
         val isBlank = userName.value.isNullOrBlank()
         Log.i("isBlank", isBlank.toString())
         _isEnabled.value = !isBlank
-    }
-
-//    var user: User
-
-    private suspend fun insert(user: User) {
-        database.insert(user)
     }
 
 }
