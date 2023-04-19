0f2a215f516cdd7552332a93b8870705ffac3499
hiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-04-19 19:21:06 +0900

    update：管理者画面からユーザー一覧画面への遷移を追加

--- app/src/main/java/com/nokopi/marketregistersystem/admin/AdminActivity.kt
+++ app/src/main/java/com/nokopi/marketregistersystem/admin/AdminActivity.kt
@@ -2,6 +2,7 @@ package com.nokopi.marketregistersystem.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import com.nokopi.marketregistersystem.R
 
 class AdminActivity : AppCompatActivity() {
@@ -9,10 +10,15 @@ class AdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, AdminFragment.newInstance())
-                .commitNow()
+        onBackPressedDispatcher.addCallback(callback)
+    }
+
+    private val callback = object : OnBackPressedCallback(true) {
+        //コールバックのhandleOnBackPressedを呼び出して、戻るキーを押したときの処理を記述
+        override fun handleOnBackPressed() {
+            // do nothing
+            return
         }
     }
+
 }
--- app/src/main/java/com/nokopi/marketregistersystem/admin/AdminFragment.kt
+++ app/src/main/java/com/nokopi/marketregistersystem/admin/AdminFragment.kt
@@ -2,30 +2,44 @@ package com.nokopi.marketregistersystem.admin
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.nokopi.marketregistersystem.R
+import com.nokopi.marketregistersystem.databinding.FragmentAdminBinding
 
 class AdminFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AdminFragment()
-    }
-
+    private lateinit var binding: FragmentAdminBinding
     private lateinit var viewModel: AdminViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this)[AdminViewModel::class.java]
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_admin, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_admin, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        viewModel = ViewModelProvider(this)[AdminViewModel::class.java]
+        binding.adminViewModel = viewModel
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.goAllUser.observe(viewLifecycleOwner) {
+            Log.i("observe goAllUser", it.toString())
+            if (it) {
+                val action = AdminFragmentDirections.actionAdminFragmentToAllUserFragment()
+                findNavController().navigate(action)
+                viewModel.goAllUserCompleted()
+            }
+        }
+
     }
 
 }
