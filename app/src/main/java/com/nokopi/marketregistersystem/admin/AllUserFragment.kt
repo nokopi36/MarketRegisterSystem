b377b5c336510a564a8db0ecc7feca4b9d6b0d6e
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2024-02-15 18:32:25 +0900

    update：顧客情報をcsvファイルに出力する機能追加

--- app/src/main/java/com/nokopi/marketregistersystem/admin/AllUserFragment.kt
+++ app/src/main/java/com/nokopi/marketregistersystem/admin/AllUserFragment.kt
@@ -1,12 +1,21 @@
 package com.nokopi.marketregistersystem.admin
 
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.DocumentsContract
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,12 +24,40 @@ import com.nokopi.marketregistersystem.R
 import com.nokopi.marketregistersystem.data.User
 import com.nokopi.marketregistersystem.data.UserDatabase
 import com.nokopi.marketregistersystem.databinding.FragmentAllUserBinding
+import java.io.File
+import java.io.IOException
 
 class AllUserFragment : Fragment() {
 
     private var _binding: FragmentAllUserBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: AllUserViewModel
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+            result: ActivityResult ->
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val resultData = result.data
+            if (resultData != null) {
+                val uri: Uri? = resultData.data
+                val csvData = makeCSVData(viewModel.userList)
+                Log.i("userList", viewModel.userList.value.toString())
+                Log.i("csvdata", csvData)
+                try {
+                    if (uri != null) {
+                        context?.contentResolver?.openOutputStream(uri).use {outputStream ->
+                            outputStream?.writer().use { writer ->
+                                writer?.write(csvData)
+                            }
+                        }
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +70,7 @@ class AllUserFragment : Fragment() {
         val dataSource = UserDatabase.getInstance(application).userDatabaseDao
         val viewModelFactory = AllUserViewModelFactory(dataSource)
         viewModel = ViewModelProvider(this, viewModelFactory)[AllUserViewModel::class.java]
+        binding.allUserViewModel = viewModel
 
         return binding.root
     }
@@ -49,9 +87,40 @@ class AllUserFragment : Fragment() {
             }
         })
 
+        viewModel.getAllUser()
+
         viewModel.userList.observe(viewLifecycleOwner) { users ->
             users.let {
                 adapter.submitList(users)
+            }
+        }
+
+        viewModel.pushBackup.observe(viewLifecycleOwner) {
+            Log.i("go pushbackup","yes")
+            if (it) {
+                AlertDialog.Builder(requireContext())
+                    .setTitle("バックアップの作成")
+                    .setMessage("全ユーザーの情報をallUsers.csvに出力します")
+                    .setPositiveButton("OK") { _, _ ->
+                        val backupFile = File(requireContext().getExternalFilesDir(null), "allUsers.csv")
+                        backupFile.bufferedWriter().use { writer ->
+                            viewModel.userList.value?.forEach { users ->
+                                val line = "${users.id},${users.userId},${users.userName},${users.userBalance}\n"
+                                writer.write(line)
+                            }
+                        }
+
+                        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+                            addCategory(Intent.CATEGORY_OPENABLE)
+                            type = "text/csv"
+                            putExtra(Intent.EXTRA_TITLE, "allUsers.csv")
+                        }
+                        resultLauncher.launch(intent)
+                    }
+                    .setNegativeButton("No") { dialog, _ ->
+                        dialog.cancel()
+                    }
+                    .show()
             }
         }
 
@@ -64,6 +133,14 @@ class AllUserFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun makeCSVData(userList: LiveData<List<User>>): String {
+        val stringBuilder = StringBuilder()
+        userList.value?.forEach { users ->
+            stringBuilder.append("${users.id},${users.userId},${users.userName},${users.userBalance}\n")
+        }
+        return stringBuilder.toString()
     }
 
 }
--- app/src/main/java/com/nokopi/marketregistersystem/admin/AllUserViewModel.kt
+++ app/src/main/java/com/nokopi/marketregistersystem/admin/AllUserViewModel.kt
@@ -1,13 +1,40 @@
 package com.nokopi.marketregistersystem.admin
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.nokopi.marketregistersystem.data.User
 import com.nokopi.marketregistersystem.data.UserDatabaseDao
+import kotlinx.coroutines.launch
 
-class AllUserViewModel(database: UserDatabaseDao) : ViewModel() {
+class AllUserViewModel(private val database: UserDatabaseDao) : ViewModel() {
 
-    var userList: LiveData<List<User>> = database.getUserName()
+    private val _userList: MutableLiveData<List<User>> = MutableLiveData<List<User>>()
+    val userList: LiveData<List<User>>
+        get() = _userList
+
+    private val _pushBackup:MutableLiveData<Boolean> = MutableLiveData<Boolean>()
+    val pushBackup: LiveData<Boolean>
+        get() = _pushBackup
+
+    fun getAllUser() {
+        viewModelScope.launch {
+            _userList.value = database.getAllUser()
+        }
+        Log.i("getAllUser","get all users")
+    }
+
+    fun pushBackup() {
+        Log.i("in pushBackup","yes")
+        _pushBackup.value = true
+        Log.i("pushbackup",pushBackup.toString())
+    }
+
+
+
+//    var userList: LiveData<List<User>> = database.getAllUser()
 
 
 
